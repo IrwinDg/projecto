38141a3b154ea15d8aeaf1f29b254d98d94ad3d0
IrwinDg <irwinrod@gmail.com>
2025-07-30 21:59:42 -0500

    nltk en el archivo main

---
+++ main.py
@@ -0,0 +1,9 @@
+#importar la libreria nltk
+import nltk
+
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+
+# definir la ruta donde se almacenaran los datos descargados de nltk
+nltk.data.path.append('C:/Users/irwin/AppData/Roaming/nltk_data')
+git commit -m "se guardaron todas stopwords en el disco local c"
